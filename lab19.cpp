4f5f9ce8075b23f93f0def6c5aa6781291725d61
Thanpitchapaiboon <thanpitchapaiboon@gmail.com>
2025-02-14 18:03:45 +0700

    19

--- lab19.cpp
+++ lab19.cpp
@@ -1,5 +1,157 @@
-// Please include header (lab21.h) here.
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
 
+using namespace std;
+
+class Unit {
+private:
+    string name;
+    string type;
+    int hp;
+    int hpmax;
+    int atk;
+    int def;
+    bool guard_on;
+
+public:
+    void create(string t);
+    void showStatus();
+    void newTurn();
+    int attack(Unit &target);
+    int beAttacked(int damage);
+    int heal();
+    void guard();
+    bool isDead();
+};
+void Unit::create(string t) {
+    if (t == "Hero") {
+        type = "Hero";
+        cout << "Please input your name: ";
+        getline(cin, name);
+        hpmax = rand() % 20 + 90;
+        atk = rand() % 5 + 14;
+        def = rand() % 3 + 9;
+    } else if (t == "Monster") {
+        type = "Monster";
+        name = "Kraken";
+        hpmax = rand() % 20 + 200;
+        atk = rand() % 5 + 25;
+        def = rand() % 3 + 5;
+    }
+    hp = hpmax;
+    guard_on = false;
+}
+void Unit::showStatus() {
+    if (type == "Hero") {
+        cout << "---------------------------------------\n";
+        cout << name << "\n";
+        cout << "HP: " << hp << "/" << hpmax << "\tATK: " << atk << "\t\tDEF: " << def;
+        cout << "\n---------------------------------------\n";
+    } else if (type == "Monster") {
+        cout << "\t\t\t\t---------------------------------------\n";
+        cout << "\t\t\t\t" << name << "\n";
+        cout << "\t\t\t\tHP: " << hp << "\t\tATK: " << atk << "\t\tDEF: " << def;
+        cout << "\n\t\t\t\t---------------------------------------\n";
+    }
+}
+
+void Unit::newTurn() {
+    guard_on = false;
+}
+
+bool Unit::isDead() {
+    return hp <= 0;
+}
+
+void Unit::guard() {
+    guard_on = true;
+    def += 5; 
+}
+
+int Unit::heal() {
+    int healAmount = rand() % 15 + 10;  
+    hp += healAmount;
+    if (hp > hpmax) {
+        hp = hpmax; 
+    }
+    return healAmount;
+}
+
+int Unit::beAttacked(int damage) {
+    int finalDamage = damage;
+    if (guard_on) {
+        finalDamage -= def;  
+        guard_on = false;    
+    } else {
+        finalDamage -= def;  
+    }
+    if (finalDamage < 0) finalDamage = 0;  
+    hp -= finalDamage;
+    return finalDamage;
+}
+
+int Unit::attack(Unit &target) {
+    int damage = atk + rand() % 5 - 2;  
+    if (target.isDead()) {
+        return 0;  
+    }
+    return target.beAttacked(damage); 
+}
+
+void drawScene(char p_action, int p, char m_action, int m) {
+    cout << "                                                       \n";
+    if (p_action == 'A') {
+        cout << "                                       " << -p << "\n";
+    } else {
+        cout << "                                                       \n";
+    }
+    cout << "                                *               *      \n";
+    cout << "                                **  *********  **      \n";
+    cout << "                                ****         ****      \n";
+    if (m_action == 'A') {
+        cout << "                 " << setw(5) << -m << "           *** **   ** ***       Attack!\n";
+    } else if (m_action == 'G') {
+        cout << "                                 *** **   ** ***       Guard!\n";
+    } else {
+        cout << "                                 *** **   ** ***       \n";
+    }
+    cout << "                                  ** **   ** **        \n";
+    cout << "                   ***             *         *         \n";
+    if (p_action == 'A') {
+        cout << "        Attack!    ***  *           *********          \n";
+    } else if (p_action == 'H') {
+        cout << "      Heal! +" << setw(2) << p << "    ***  *           *********          \n";
+    } else if (p_action == 'G') {
+        cout << "         Guard!    ***  *           *********          \n";
+    } else {
+        cout << "                   ***  *           *********          \n";
+    }
+    cout << "                    *  *       ***  *  *  *            \n";
+    cout << "                  *****           **   *   *           \n";
+    cout << "                    *                  *    ***        \n";
+    cout << "                   * *               **        *       \n";
+    cout << "                  *   *                                \n";
+    cout << "                                                       \n";
+}
+
+void playerWin() {
+    cout << "*******************************************************\n";
+    for (int i = 0; i < 3; i++) cout << "*                                                     *\n";
+    cout << "*                   YOU WIN!!!                        *\n";
+    for (int i = 0; i < 3; i++) cout << "*                                                     *\n";
+    cout << "*******************************************************\n";
+}
+
+void playerLose() {
+    cout << "*******************************************************\n";
+    cout << "*                                                     *\n";
+    cout << "*                   YOU LOSE!!!                       *\n";
+    cout << "*                                                     *\n";
+    cout << "*******************************************************\n";
+}
 int main(){
 	srand(time(0));
 	Unit hero,mons;
